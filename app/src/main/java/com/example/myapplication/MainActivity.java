cf81177e3b49f4bddb8e65a3e16e881ab4ab16ce
sjieys <0wonyounglee0@gmail.com>
2024-08-19 17:19:55 +0900

    First Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+private ActivityMainBinding binding;
+private  int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btn.setOnClickListener(v->{
+            count++;
+            binding.textView.setText(String.valueOf(count));
+        });
+    }
+}
